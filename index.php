cde123fdd6ba18a1c67781bd85939b650c8aff1c
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-01-04 13:52:22 -0500

    Index is now a php file with an array defined in the body.

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Pair Program 1</title>
+</head>
+<body>
+        <h1>Pair Program 1</h1>
+        <?php
+            $numbers = array("7","9","8","9","8","8","6");
+        ?>
+
+
+</body>
+</html>
