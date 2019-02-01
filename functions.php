9f23e97259c6655628a6105a7e80866bc2b981a2
Collin Woodruff <Cwoodruff@mail.greenriver.edu>
2019-02-01 15:48:58 -0500

    Created a few new functions

--- functions.php
+++ functions.php
@@ -6,12 +6,48 @@
  * Time: 10:24 AM
  */
 
+/**
+ * @param $numbers
+ */
 function printR($numbers){
-    echo " '" . implode("','",$numbers) . "' ";
+    echo implode(",", $numbers);
 }
 
+/**
+ * @param $numbers
+ */
 function largest($numbers){
-    max($numbers);
+    echo max($numbers);
 }
 
-printR();
+/**
+ * @param $numbers
+ */
+function average($numbers){
+    $sum = 0;
+    foreach($numbers as $number){
+        $sum += $number;
+    }
+    echo $sum/sizeof($numbers);
+}
+
+function removeDups($numbers){
+    $dups = array();
+    foreach(array_count_values($numbers) as $val => $c) {
+        if ($c > 1) $dups[] = $val;
+        echo $val . " ";
+    }
+}
+
+function distribution($numbers){
+    $dups = array();
+    echo "[";
+    foreach(array_count_values($numbers) as $val => $c) {
+        if ($c > 1) $dups[] = $val;
+        echo $val . "=>" . $c;
+        if($val != 6) {
+            echo ", ";
+        }
+    }
+    echo "]";
+}
--- index.php
+++ index.php
@@ -7,7 +7,18 @@
 <body>
         <h1>Pair Program 1</h1>
         <?php
-            $numbers = array("7","9","8","9","8","8","6");
+        require("functions.php");
+            $numbers = array(7,9,8,9,8,8,6);
+            echo "<h2>The array is:</h2>";
+            printR($numbers);
+            echo "<h2>The largest number is:</h2>";
+            largest($numbers);
+            echo "<h2>The average is:</h2>";
+            average($numbers);
+            echo "<h2>The non-duplicate array is:</h2>";
+            removeDups($numbers);
+        echo "<h2>The counting duplicate array is:</h2>";
+        distribution($numbers);
         ?>
 
 
